04d08e13af489d942904e71d6eec3c82e7dcdaaf
Yang Wang <wang701@purdue.edu>
2020-05-12 23:00:01 -0400

    Add: verification when name mismatch occurs

--- label.py
+++ label.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
 from annotator import Annotator
 
 # define the labels
-labels = [{'name': 'header-up', 'color': (0, 255, 0)},
-          {'name': 'header-down', 'color': (255, 0, 0)}]
+labels = [{'name': 'header down', 'color': (0, 255, 0)},
+          {'name': 'header up', 'color': (255, 0, 0)}]
 
 # folder for storing clips
 clips_folder = '/home/yang/research/dock/explicator/tmp-clips'
@@ -14,9 +15,11 @@ videos_path = '/home/yang/research/dock/explicator/videos-paper/left'
 
 # define video for labeling
 video_name = 'out_2019-07-15_14-36-14_fin.mp4'
+video_name_cut = video_name.strip('_fin.mp4')
 video_anno = videos_path + '/' + video_name
 
-anno_file = video_name.replace('.mp4', '-labels.json')
+anno_path = '/home/yang/research/explicator/case-6088-07152019-labels'
+anno_file = os.path.join(anno_path, video_name.replace('.mp4', '-labels.json'))
 
 # set up annotator
 annotator = Annotator(labels, clips_folder, annotation_file=anno_file)
@@ -29,7 +32,13 @@ if [f for f in os.listdir(clips_folder) if not f.startswith('.')] == []:
     # note: our video is 30 fps, 30 * 3 = 90 = clip_length
     annotator.video_to_clips(video_anno, clips_folder, clip_length=90)
 else:
-    print('Clips already generated.')
+    print('Clips folder not empty.')
+    for f in os.listdir(clips_folder):
+        if not f.startswith(video_name_cut):
+            print('Detected different clip name,', \
+                  'please verify your clips folder:\n\n\t{}\n'.format( \
+                        clips_folder))
+            sys.exit(1)
 
 # run the GUI
 annotator.main()
